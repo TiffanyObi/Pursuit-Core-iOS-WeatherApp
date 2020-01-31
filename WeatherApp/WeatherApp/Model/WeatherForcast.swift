bcd88d65994014737e9e361b4afa4bf6a8ccd182
Tiffany Obi <tiffanyobi123@gmail.com>
2020-01-31 17:15:29 -0500

    started weather project. created a model and the api client.

---
+++ WeatherApp/WeatherApp/Model/WeatherForcast.swift
@@ -0,0 +1,28 @@
+//
+//  WeatherForcast.swift
+//  WeatherApp
+//
+//  Created by Tiffany Obi on 1/31/20.
+//  Copyright © 2020 David Rifkin. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherForecast: Codable {
+    let timezone: String
+    let daily: WeekForecast
+}
+
+struct WeekForecast: Codable {
+    let data: [Details]
+}
+
+struct Details: Codable {
+    let summary: String
+    let precipType: String
+    let temperatureHigh: Double
+    let temperatureLow: Double
+    let dewPoint: Double
+    let humidity: Double
+    let windGust: Double
+}
---
+++ WeatherApp/WeatherApp/Model/WeatherForcastAPIClient.swift
@@ -0,0 +1,47 @@
+//
+//  WeatherForcastAPIClient.swift
+//  WeatherApp
+//
+//  Created by Tiffany Obi on 1/31/20.
+//  Copyright © 2020 David Rifkin. All rights reserved.
+//
+
+import Foundation
+import NetworkHelper
+
+struct WeatherAPIClient {
+    
+    static func getForcast(for zipCode: String, completion: @escaping (Result<[Details],AppError>) -> () ) {
+        
+
+        
+        let weatherForecastUrl = "https://api.darksky.net/forecast/830f4b6fb284e95da3d4baf7d280824f/\(zipCode)"
+        
+        guard let url = URL(string: weatherForecastUrl) else {
+            print(AppError.badURL(weatherForecastUrl))
+            return
+                    }
+        
+        let request = URLRequest(url: url)
+        
+        NetworkHelper.shared.performDataTask(with: request) { (result) in
+            switch result {
+                
+            case .failure(let appError):
+                completion(.failure(appError))
+                
+            case.success(let data):
+                do {
+            let weatherForcast = try JSONDecoder().decode(WeatherForecast.self, from: data)
+                    completion(.success(weatherForcast.daily.data))
+                } catch {
+                    print(AppError.decodingError(error))
+                }
+                
+            }
+        }
+        
+    }
+    
+    
+}
